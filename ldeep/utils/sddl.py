55e5de1d3809a13efd73f3b1dc5304981f8d01e7
flgy <florian.guilbert@synacktiv.com>
2019-01-08 16:46:08 +0100

    Formatting sddl file

--- ldeep/utils/sddl.py
+++ ldeep/utils/sddl.py
@@ -5,25 +5,26 @@ from struct import unpack
 from ldap3.protocol.formatters.formatters import format_sid, format_uuid
 
 SDDLTypeFlags = {
-		'Self Relative'                 : 0b1000000000000000,
-		'RM Control Valid'              : 0b0100000000000000,
-		'SACL Protected'                : 0b0010000000000000,
-		'DACL Protected'                : 0b0001000000000000,
-		'SACL Auto Inherit'             : 0b0000100000000000,
-		'DACL Auto Inherit'             : 0b0000010000000000,
-		'SACL Auto Inherit Required'    : 0b0000001000000000,
-		'DACL Auto Inherit Required'    : 0b0000000100000000,
-		'Server Security'               : 0b0000000010000000,
-		'DACL Trusted'                  : 0b0000000001000000,
-		'SACL Defaulted'                : 0b0000000000100000,
-		'SACL Present'                  : 0b0000000000010000,
-		'DACL Defaulted'                : 0b0000000000001000,
-		'DACL Present'                  : 0b0000000000000100,
-		'Group Defaulted'               : 0b0000000000000010,
-		'Owner Defaulted'               : 0b0000000000000001,
-		}
+	'Self Relative'					: 0b1000000000000000,
+	'RM Control Valid'				: 0b0100000000000000,
+	'SACL Protected'				: 0b0010000000000000,
+	'DACL Protected'				: 0b0001000000000000,
+	'SACL Auto Inherit'				: 0b0000100000000000,
+	'DACL Auto Inherit'				: 0b0000010000000000,
+	'SACL Auto Inherit Required'	: 0b0000001000000000,
+	'DACL Auto Inherit Required'	: 0b0000000100000000,
+	'Server Security'				: 0b0000000010000000,
+	'DACL Trusted'					: 0b0000000001000000,
+	'SACL Defaulted'				: 0b0000000000100000,
+	'SACL Present'					: 0b0000000000010000,
+	'DACL Defaulted'				: 0b0000000000001000,
+	'DACL Present'					: 0b0000000000000100,
+	'Group Defaulted'				: 0b0000000000000010,
+	'Owner Defaulted'				: 0b0000000000000001,
+}
 
 SID_SIZE = 28
+
 
 def parse_ntSecurityDescriptor(input_buffer):
 	""" Parses a ntSecurityDescriptor.
@@ -32,14 +33,14 @@ def parse_ntSecurityDescriptor(input_buffer):
 
 	fields = ('Revision', 'Raw Type', 'Offset to owner SID', 'Offset to group SID', 'Offset to SACL', 'Offset to DACL')
 
-	for k, v in zip(fields,unpack('<HHIIII', input_buffer[:20])):
+	for k, v in zip(fields, unpack('<HHIIII', input_buffer[:20])):
 		out[k] = v
 
 	out['Type'] = parse_sddl_type(out['Raw Type'])
 
-	for x in ('Owner', 'Group'):	
+	for x in ('Owner', 'Group'):
 		offset = out['Offset to %s SID' % (x.lower())]
-		out['%s SID' % x] = format_sid(input_buffer[offset:offset+SID_SIZE])
+		out['%s SID' % x] = format_sid(input_buffer[offset:offset + SID_SIZE])
 
 	if out['Type']['SACL Present']:
 		out['SACL'] = parse_acl(input_buffer[out['Offset to SACL']:])
@@ -49,11 +50,14 @@ def parse_ntSecurityDescriptor(input_buffer):
 
 	return out
 
+
 def resolve_flags(bfr, flags):
-	return {k : v & bfr != 0 for k, v in flags.items()}
+	return {k: v & bfr != 0 for k, v in flags.items()}
+
 
 def parse_sddl_type(typeflags):
 	return resolve_flags(typeflags, SDDLTypeFlags)
+
 
 def parse_acl(input_buffer):
 	out = dict()
@@ -62,8 +66,9 @@ def parse_acl(input_buffer):
 	for k, v in zip(fields, unpack('<HHI', input_buffer[:8])):
 		out[k] = v
 
-	out['ACEs'] = parse_aces(input_buffer[8:8+out['Size']], out['Num ACEs'])
+	out['ACEs'] = parse_aces(input_buffer[8:8 + out['Size']], out['Num ACEs'])
 	return out
+
 
 def parse_aces(input_buffer, count):
 	out = []
@@ -87,77 +92,83 @@ def parse_aces(input_buffer, count):
 
 			offset = 12
 			if ace['Object Flags']['Object Type Present']:
-				ace['GUID'] = format_uuid(input_buffer[offset:offset+16])
+				ace['GUID'] = format_uuid(input_buffer[offset:offset + 16])
 				offset += 16
 			if ace['Object Flags']['Inherited Object Type Present']:
-				ace['Inherited GUID'] = format_uuid(input_buffer[offset:offset+16])
+				ace['Inherited GUID'] = format_uuid(input_buffer[offset:offset + 16])
 				offset += 16
 
 			ace['SID'] = format_sid(input_buffer[offset:ace['Size']])
 
 		ace['SID'] = format_sid(input_buffer[8:ace['Size']])
-			
+
 		input_buffer = input_buffer[ace['Size']:]
 
 		out.append(ace)
 	return out
 
+
 ACEAccessFlags = {
-		'Generic Read'       : 0b10000000000000000000000000000000,
-		'Generic Write'      : 0b01000000000000000000000000000000,
-		'Generic Execute'    : 0b00100000000000000000000000000000,
-		'Generic All'        : 0b00010000000000000000000000000000,
-		'Maximum Allowed'    : 0b00000010000000000000000000000000,
-		'Access SACL'        : 0b00000000100000000000000000000000,
-		'Synchronise'        : 0b00000000000100000000000000000000,
-		'Write Owner'        : 0b00000000000010000000000000000000,
-		'Write DAC'          : 0b00000000000001000000000000000000,
-		'Read Control'       : 0b00000000000000100000000000000000,
-		'Delete'             : 0b00000000000000010000000000000000,
-		'Ads Control Access' : 0b00000000000000000000000100000000,
-		'Ads List Object'    : 0b00000000000000000000000010000000,
-		'Ads Delete Tree'    : 0b00000000000000000000000001000000,
-		'Ads Write Prop'     : 0b00000000000000000000000000100000,
-		'Ads Read Prop'      : 0b00000000000000000000000000010000,
-		'Ads Self Write'     : 0b00000000000000000000000000001000,
-		'Ads List'           : 0b00000000000000000000000000000100,
-		'Ads Delete Child'   : 0b00000000000000000000000000000010,
-		'Ads Create Child'   : 0b00000000000000000000000000000001
-		}
+	'Generic Read'			: 0b10000000000000000000000000000000,
+	'Generic Write'			: 0b01000000000000000000000000000000,
+	'Generic Execute'		: 0b00100000000000000000000000000000,
+	'Generic All'			: 0b00010000000000000000000000000000,
+	'Maximum Allowed'		: 0b00000010000000000000000000000000,
+	'Access SACL'			: 0b00000000100000000000000000000000,
+	'Synchronise'			: 0b00000000000100000000000000000000,
+	'Write Owner'			: 0b00000000000010000000000000000000,
+	'Write DAC'				: 0b00000000000001000000000000000000,
+	'Read Control'			: 0b00000000000000100000000000000000,
+	'Delete'				: 0b00000000000000010000000000000000,
+	'Ads Control Access'	: 0b00000000000000000000000100000000,
+	'Ads List Object'		: 0b00000000000000000000000010000000,
+	'Ads Delete Tree'		: 0b00000000000000000000000001000000,
+	'Ads Write Prop'		: 0b00000000000000000000000000100000,
+	'Ads Read Prop'			: 0b00000000000000000000000000010000,
+	'Ads Self Write'		: 0b00000000000000000000000000001000,
+	'Ads List'				: 0b00000000000000000000000000000100,
+	'Ads Delete Child'		: 0b00000000000000000000000000000010,
+	'Ads Create Child'		: 0b00000000000000000000000000000001
+}
+
 
 def parse_ace_access(input_buffer):
 	return resolve_flags(input_buffer, ACEAccessFlags)
 
+
 ACEObjectFlags = {
-		'Object Type Present'   : 0b00000000000000000000000000000010,
-		'Inherited Object Type Present'   : 0b00000000000000000000000000000001
-		}
+	'Object Type Present'				: 0b00000000000000000000000000000010,
+	'Inherited Object Type Present'		: 0b00000000000000000000000000000001
+}
+
 
 def parse_ace_object_flags(input_buffer):
 	return resolve_flags(input_buffer, ACEObjectFlags)
 
+
 ACEType = {
-		0x00: 'Access Allowed',
-		0x01: 'Access Denied',
-		0x02: 'System Audit',
-		0x03: 'System Alarm',
-		0x04: 'Access Allowed Compound',
-		0x05: 'Access Allowed Object',
-		0x06: 'Access Denied Object',
-		0x07: 'System Audit Object',
-		0x08: 'System Alarm Object',
-		0x09: 'Access Allowed Callback',
-		0x0A: 'Access Denied Callback',
-		0x0B: 'Access Allowed Callback Object',
-		0x0C: 'Access Denied Callback Object',
-		0x0D: 'System Audit Callback',
-		0x0E: 'System Alarm Callback',
-		0x0F: 'System Audit Callback Object',
-		0x10: 'System Alarm Callback Object',
-		0x11: 'System Mandatory Label',
-		0x12: 'System Resource Attribute',
-		0x13: 'System Scoped Policy ID'
-		}
+	0x00: 'Access Allowed',
+	0x01: 'Access Denied',
+	0x02: 'System Audit',
+	0x03: 'System Alarm',
+	0x04: 'Access Allowed Compound',
+	0x05: 'Access Allowed Object',
+	0x06: 'Access Denied Object',
+	0x07: 'System Audit Object',
+	0x08: 'System Alarm Object',
+	0x09: 'Access Allowed Callback',
+	0x0A: 'Access Denied Callback',
+	0x0B: 'Access Allowed Callback Object',
+	0x0C: 'Access Denied Callback Object',
+	0x0D: 'System Audit Callback',
+	0x0E: 'System Alarm Callback',
+	0x0F: 'System Audit Callback Object',
+	0x10: 'System Alarm Callback Object',
+	0x11: 'System Mandatory Label',
+	0x12: 'System Resource Attribute',
+	0x13: 'System Scoped Policy ID'
+}
+
 
 def parse_sddl_dacl_ace_type(ace_type):
 	return ACEType[ace_type]
